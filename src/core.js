e0f3b24197f000cc09aefc97169c7ab937ca310b
David Skx <david.skx@posteo.de>
2015-07-20 02:06:25 +0200

    refactored vars

--- src/core.js
+++ src/core.js
@@ -4,68 +4,57 @@ var a = "bcdefdcbaaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxAAAAAAAABCDEFDCB".split
 /**
  *
  * @param e
- * @param f figure select / figure about to be moved
- * @param FName origin name
- * @param fName desitination name
- * @param allowMove allow figure to move
- * @param FIndex origin-index
- * @param fIndex destination-index
- * @param {"b"|"w"} fType type of the current figure (b=black, w=white)
- * @param {"b"|"w"} FType type of the destinations figure (b=black, w=white)
- * @param same destination equals origin
- * @param fDiff
  * @param kill
+ * @param n
  */
-p.onclick = function (e, f, FName, fName, allowMove, FIndex, fIndex, fType, FType, same, fDiff, kill) {
+p.onclick = function (e, kill, n) {
 	f = e.target;
 
 	if (F) {
 		// move to a field
-		FName = F.tagName;
-		fName = f.tagName;
-		allowMove = 0;
-		FIndex = F[i = "getAttribute"]('i');
-		fIndex = f[i]('i');
-		FType = f[i]('c');
-		fType = F[i]('c');
-		same = FIndex == fIndex;
-		fDiff = Math.abs(FIndex - fIndex);
+		N = F.tagName;					// origin name
+		n = f.tagName;					// destination name
+		m = 0;							// allow movement
+		J = F[i = "getAttribute"]('i');	// index of origin
+		j = f[i]('i');					// index of destination
+		T = f[i]('c');					// origin type
+		t = F[i]('c');					// destination type
+		s = J == j;						// same field?
+		d = Math.abs(J - j);			// difference between origin and destination
 
-		if (!same) {
+		if (!s) {
 			//console.log("try " + F.tagName + " from " + FIndex + " to " + fIndex, fType);
 
-			if (FName == 'A') { // pawn
-				if (fName == 'X') { // moving
-					if (fType == 'w' && FIndex - fIndex == -8 || FIndex > 47 && fDiff == 16) allowMove = 1;	// white
-					if (fType == 'b' && FIndex - fIndex == 8 || FIndex < 16 && fDiff == 16) allowMove = 1;	// black
+			if (N == 'A') { // pawn
+				if (n == 'X') { // moving
+					if (t == 'w' && J - j == -8 || J > 47 && d == 16) m = 1;	// white
+					if (t == 'b' && J - j == 8 || J < 16 && d == 16) m = 1;	// black
 				} else { // killing
-					if (fDiff == 7 || fDiff == 9) allowMove = 1;
+					if (d == 7 || d == 9) m = 1;
 				}
 			} else {
 				// bishop | queen
-				if ((FName == 'D' || FName == 'E') && !(fDiff % 7 && fDiff % 9)) allowMove = 1;
+				if ((N == 'D' || N == 'E') && !(d % 7 && d % 9)) m = 1;
 
 				// rook | queen
-				if ((FName == 'B' || FName == 'E') && (!(fDiff % 8) || (~~(FIndex / 8) == ~~(fIndex / 8)))) allowMove = 1;
+				if ((N == 'B' || N == 'E') && (!(d % 8) || (~~(J / 8) == ~~(j / 8)))) m = 1;
 
 				// king
-				if (FName == 'F' && (fDiff == 1 || (fDiff > 6 && fDiff < 10 ))) allowMove = 1;
+				if (N == 'F' && (d == 1 || (d > 6 && d < 10 ))) m = 1;
 
 				// knight
-				if (FName == 'C' && (fDiff == 17 || fDiff == 15 || fDiff == 10 || fDiff == 6)) allowMove = 1;
+				if (N == 'C' && (d == 17 || d == 15 || d == 10 || d == 6)) m = 1;
 			}
 
-			if (fName != 'X' && fType != FType) {
+			if (n != 'X' && t != T) {
 				kill = 1;
 			}
 
-			if (allowMove) {
-				a[fIndex] = fType == 'w' ? FName.toLowerCase() : FName;
-				a[FIndex] = 'x';
+			if (m) {
+				a[j] = t == 'w' ? N.toLowerCase() : N;
+				a[J] = 'x';
 
 				F = 0;
-
-				//d()
 			}
 		}
 	}
@@ -77,6 +66,6 @@ p.onclick = function (e, f, FName, fName, allowMove, FIndex, fIndex, fType, FTyp
 
 	p.innerHTML = s;
 
-	F = fName != 'X' && !kill ? f : F;
+	F = n != 'X' && !kill ? f : F;
 };
 p.onclick({});
