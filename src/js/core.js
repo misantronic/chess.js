32b695d32f6a83f41e308b3992197183c6d13a51
misantronic <david.skx@posteo.de>
2015-07-24 17:02:41 +0200

    reformatting

--- src/js/core.js
+++ src/js/core.js
@@ -17,14 +17,14 @@
 var a = "bcdfedcbaaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxAAAAAAAABCDFEDCB".split(""), F, P = 0;
 
 // prepare HTML-container
-document.body[H="innerHTML"]='<x id=p />';
+document.body[H = "innerHTML"] = '<x id=p />';
 
 /**
  * @param {Event|String|Object} e event, placeholder
  * @param {Number|Boolean} k [kill]
  * @param {String} n [destination name]
  */
-p.onclick = function (e, k, n) {
+p.onclick = function(e, k, n) {
 	f = e.target;							// set destination
 	T = f ? f[i = "getAttribute"]('c') : 0;	// origin type
 
@@ -52,17 +52,17 @@ p.onclick = function (e, k, n) {
 			} else {
 				// bishop | queen
 				if (N == 'D' || N == 'E')
-					// dertermine direction move-set
-					if(M = !(d % 7) ? 7 : !(d % 9) ? 9 : 0)
-						// move
+				// dertermine direction move-set
+					if (M = !(d % 7) ? 7 : !(d % 9) ? 9 : 0)
+					// move
 						for (m = 1, e = J > j ? j + M : J + M; e < (J > j ? J : j); e += M) if (a[e] != "x") m = 0;
 
 				// rook | queen
-				if(N == 'B' || N == 'E')
-					// dertermine horizontal or vertial move-set
-					if(M = ~~(J / 8) == ~~(j / 8) ? 1 : !(d % 8) ? 8 : 0)
-						// move
-						for (m = 1, e = J < j ? J+M : J-M; J < j ? e < j : e > j; J < j ? e+=M : e-=M) if (a[e] != "x") m = 0;
+				if (N == 'B' || N == 'E')
+				// dertermine horizontal or vertial move-set
+					if (M = ~~(J / 8) == ~~(j / 8) ? 1 : !(d % 8) ? 8 : 0)
+					// move
+						for (m = 1, e = J < j ? J + M : J - M; J < j ? e < j : e > j; J < j ? e += M : e -= M) if (a[e] != "x") m = 0;
 
 				if (
 					(N == 'F' && (d == 1 || (d > 6 && d < 10 ))) || 			// king
@@ -86,8 +86,7 @@ p.onclick = function (e, k, n) {
 	}
 
 	// draw field
-	p[H] = "";
-	for (i = 0; i < a.length, e=a[i]; i++) {
+	for (p[H] = "", i = 0; i < a.length, e = a[i]; i++) {
 		p[H] += '<' + e + ' c=' + (e.charCodeAt(0) < 97 ? 1 : 0) + ' i=' + i + '></' + e + '>';
 	}
 
